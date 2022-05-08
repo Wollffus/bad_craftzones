6afe0eb036e578a1b8f3c83846b24bce8c19ad11
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-05-07 23:34:23 -0700

    Update server.lua

--- bad_craftzones/server.lua
+++ bad_craftzones/server.lua
@@ -1,83 +1,85 @@
 wollffus = {}
+-------------------------------------------------------------------------------------------C O O K I N G----------------------------------------------------------------------------------------
 TriggerEvent("redemrp_inventory:getData",function(call)
     wollffus = call
-    wollffus.CreateCraftingStation("public_cooking_1", Config.craftingcoordsone, Config.craftingtypeone , "empty")
+    wollffus.CreateCraftingStation("public_cooking_1", -361.32,748.25,116.47, "cooking" , "empty")
     wollffus.updateCraftings(-1)
 end)
 
 wollffus = {}
 TriggerEvent("redemrp_inventory:getData",function(call)
     wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordstwo, Config.craftingtypetwo , "empty")
+    wollffus.CreateCraftingStation("public_cooking_2", -845.46, -12163.09, 43.53, "cooking" , "empty")
     wollffus.updateCraftings(-1)
 end)
 
 wollffus = {}
 TriggerEvent("redemrp_inventory:getData",function(call)
     wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordsthree, Config.craftingtypethree , "empty")
+    wollffus.CreateCraftingStation("public_cooking_3", 1305.31, -1135.54, 81.4, "cooking" , "empty")
+    wollffus.updateCraftings(-1)
+end)
+-------------------------------------------------------------------------------------------B L A C K S M I T H----------------------------------------------------------------------------------------
+wollffus = {}
+TriggerEvent("redemrp_inventory:getData",function(call)
+    wollffus = call
+    wollffus.CreateCraftingStation("public_blacksmith_1", -2592.69, 408.19, 148.88, "blacksmith" , "empty")
     wollffus.updateCraftings(-1)
 end)
 
 wollffus = {}
 TriggerEvent("redemrp_inventory:getData",function(call)
     wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordsfour, Config.craftingtypefour , "empty")
+    wollffus.CreateCraftingStation("public_blacksmith_2", 1327.39, -1321.93, 77.94, "blacksmith" , "empty")
     wollffus.updateCraftings(-1)
 end)
 
 wollffus = {}
 TriggerEvent("redemrp_inventory:getData",function(call)
     wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordsfive, Config.craftingtypefive , "empty")
+    wollffus.CreateCraftingStation("public_blacksmith_3", -869.84, -1394.73, 43.53, "blacksmith" , "empty")
     wollffus.updateCraftings(-1)
 end)
 
 wollffus = {}
 TriggerEvent("redemrp_inventory:getData",function(call)
     wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordssix, Config.craftingtypesix , "empty")
+    wollffus.CreateCraftingStation("public_blacksmith_4", 2710.59, -1287.5, 49.64, "blacksmith" , "empty")
     wollffus.updateCraftings(-1)
 end)
 
 wollffus = {}
 TriggerEvent("redemrp_inventory:getData",function(call)
     wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordsseven, Config.craftingtypeseven , "empty")
+    wollffus.CreateCraftingStation("public_blacksmith_5", -277.21, 779.03, 119.55 , "blacksmith" , "empty")
+    wollffus.updateCraftings(-1)
+end)
+-------------------------------------------------------------------------------------------C R A F T I N G----------------------------------------------------------------------------------------
+wollffus = {}
+TriggerEvent("redemrp_inventory:getData",function(call)
+    wollffus = call
+    wollffus.CreateCraftingStation("public_crafting_1", -369.41, 796.6, 116.29, "crafting" , "empty")
     wollffus.updateCraftings(-1)
 end)
 
 wollffus = {}
 TriggerEvent("redemrp_inventory:getData",function(call)
     wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordseight, Config.craftingtypeeight , "empty")
+    wollffus.CreateCraftingStation("public_crafting_2", 1301.56, -1133.58, 81.36, "crafting" , "empty")
+    wollffus.updateCraftings(-1)
+end)
+
+
+wollffus = {}
+TriggerEvent("redemrp_inventory:getData",function(call)
+    wollffus = call
+    wollffus.CreateCraftingStation("public_crafting_3", -1823.56, -424.86, 159.96 , "crafting" , "empty")
     wollffus.updateCraftings(-1)
 end)
 
 wollffus = {}
 TriggerEvent("redemrp_inventory:getData",function(call)
     wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordsnine, Config.craftingtypenine , "empty")
-    wollffus.updateCraftings(-1)
-end)
-
-wollffus = {}
-TriggerEvent("redemrp_inventory:getData",function(call)
-    wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordsten, Config.craftingtypeten , "empty")
-    wollffus.updateCraftings(-1)
-end)
-
-wollffus = {}
-TriggerEvent("redemrp_inventory:getData",function(call)
-    wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordseleven, Config.craftingtypeeleven , "empty")
-    wollffus.updateCraftings(-1)
-end)
-
-wollffus = {}
-TriggerEvent("redemrp_inventory:getData",function(call)
-    wollffus = call
-    wollffus.CreateCraftingStation("public_crafting_1", Config.craftingcoordstwelve, Config.craftingtypetwelve , "empty")
+    wollffus.CreateCraftingStation("public_crafting_4", 2764.25, -1135.4, 48.64, "crafting" , "empty")
     wollffus.updateCraftings(-1)
 end)
